5de819758c03c2ab11517e0dac996930724a30d7
serhatprogramming <serhatprogramming@gmail.com>
2023-08-23 23:37:44 -0400

    hide show toggle

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,11 +1,13 @@
-const Playlist = ({ playlist }) => {
+const Playlist = ({ playlist, handleLike }) => {
   return (
     <div>
       <div>
         {playlist.name} by {playlist.creator}
       </div>
       <div>{playlist.numOfSongs} songs</div>
-      <div>{playlist.likes} likes</div>
+      <div>
+        {playlist.likes} likes <button onClick={handleLike}>like</button>
+      </div>
       <div className="playlist-owner">
         Added by <em>{playlist.user.username}</em>
       </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,27 @@
+import React, { useState } from "react";
+
 const Section = ({ componentTitle, children }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
     <div className="section">
-      <strong>{componentTitle}</strong>
-      {children}
+      <strong>{showDetails ? null : componentTitle}</strong>
+      {showDetails ? (
+        <>
+          {children}
+          <button className="small-button" onClick={toggleDetails}>
+            Hide Details
+          </button>
+        </>
+      ) : (
+        <button className="small-button" onClick={toggleDetails}>
+          Show Details
+        </button>
+      )}
     </div>
   );
 };
